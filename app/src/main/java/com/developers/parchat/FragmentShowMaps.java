de6259953c13019c401566280747fb06cd4b4793
daveloper32 <80218906+daveloper32@users.noreply.github.com>
2021-07-28 15:49:46 -0500

    implementacion obtenerUbicacion_Direccion

--- app/src/main/java/com/developers/parchat/FragmentShowMaps.java
+++ app/src/main/java/com/developers/parchat/FragmentShowMaps.java
@@ -1,9 +1,12 @@
 package com.developers.parchat;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,22 +17,33 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.List;
 
 public class FragmentShowMaps extends Fragment {
 
     private GoogleMap mMap;
 
-    // Pizza Candelaria 4.596216, -74.069437
-    // Nativa Arte y Comida Natural 4.596735, -74.070038
+    // Pizza Candelaria 4.596205, -74.069449
+    // Nativa Arte y Comida Natural 4.596744, -74.070033
     // Restaurante Rosita 4.597280, -74.069721
     // Natsuki Sushi Bar 4.596704, -74.069411
     // Creamos e inicializamos un arreglo de 2 dimensiones tipo double para guardar latitudes y longitudes
-    private double coordenadas[][] = {{4.596216, 4.596735, 4.597280, 4.596704},
-            {-74.069437, -74.070038, -74.069721, -74.069411}};
+    private double coordenadas[][] = {{4.596205, 4.596744, 4.597280, 4.596704},
+            {-74.069449, -74.070033, -74.069721, -74.069411}};
     // Creamos e inicializamos un arreglo tipo String con los nombres de los restaurantes
-    private String nomRestaurantes[] = {"Pizza Candelaria", "Nativa Arte y Comida Natural",
-            "Restaurante Rosita", "Natsuki Sushi Bar"};
+    private String nomRestaurantes[] = {"Tamarine", "Restaurante Peruano - El Indio de Machu Picchu",
+            "Restaurante 69 Gauchos",
+            "Harry Sasson"};
+    private String dirRestaurantes[] = {"Calle 73 # 8 - 60 Dentro del Hotel JW Marriott Bogotá, Bogotá, Cundinamarca",
+            "Carrera 10 # 72 - 32, Localidad de Chapinero, Bogotá",
+            "Calle 69a #10 - 16, Bogotá, Cundinamarca",
+            "Carrera 9 # 75 - 70, Bogotá"};
     // Creamos un arreglo de objetos LatLong de 4 posiciones
     private LatLng restaurantes[] = new LatLng[4];
 
@@ -54,19 +68,51 @@ public class FragmentShowMaps extends Fragment {
                 // Recorremos el arreglo de objetos LatLng para ubicar y nombrar marcadores con los restauantes en el mapa
                 for (int x = 0; x < restaurantes.length; x++) {
                     // Creamos un objeto LatLng y le pasamos las coordenadas de x restaurante
-                    restaurantes[x] = new LatLng(coordenadas[0][x], coordenadas[1][x]);
+                    //restaurantes[x] = new LatLng(coordenadas[0][x], coordenadas[1][x]);
+                    restaurantes[x] = obtenerUbicacion_Direccion(dirRestaurantes[x]);
                     // Añadimos un marcador al mapa en las coordenadas de x restaurante
                     mMap.addMarker(new MarkerOptions().
                             position(restaurantes[x])
                             .title(nomRestaurantes[x])
-                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_maps_marker_restaurante))
+                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_mapa_restaurante))
                             .anchor(0.0f, 0.0f));
+                    mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                        @Override
+                        public boolean onMarkerClick(@NonNull @NotNull Marker marker) {
+                            return false;
+                        }
+                    });
+
                 }
                 // Ubicamos la camara de google maps en el primer restaurante
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(restaurantes[0], 18));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(restaurantes[0], 15));
             }
         });
         return v;
+    }
+
+    public LatLng obtenerUbicacion_Direccion(String strAddress) {
+        //Create coder with Activity context - this
+        Geocoder coder = new Geocoder(getContext());
+        List<Address> address;
+        LatLng latLng = null;
+
+        try {
+            //Get latLng from String
+            address = coder.getFromLocationName(strAddress, 5);
+
+            if (address != null) {
+                try {
+                    Address location = address.get(0);
+                    latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                } catch (IndexOutOfBoundsException er) {
+                    Toast.makeText(getContext(), address.toString() + "Direccion no valida" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return latLng;
     }
 }
 
