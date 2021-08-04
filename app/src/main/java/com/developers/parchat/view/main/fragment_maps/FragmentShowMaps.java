ab59dc7f100bf76082bb02219c2002f726df4075
daveloper32 <80218906+daveloper32@users.noreply.github.com>
2021-08-04 18:46:13 -0500

    Se recomiendan sitios segun la posicion del marcador en un rango en km

--- app/src/main/java/com/developers/parchat/view/main/fragment_maps/FragmentShowMaps.java
+++ app/src/main/java/com/developers/parchat/view/main/fragment_maps/FragmentShowMaps.java
@@ -49,6 +49,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class FragmentShowMaps extends Fragment {
 
+    // Creamos un objeto LatLng y le pasamos las coordenadas de Bogota
+    LatLng bogota = new LatLng(4.60971, -74.08175);
+
     private GoogleMap mMap;
 
 
@@ -65,6 +68,9 @@ public class FragmentShowMaps extends Fragment {
 
     private List<String> lugaresGeoQueryResult;
 
+    private List<Marker> marcadoresMapa;
+    private LatLng latLngUbicacionUsuario;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -79,7 +85,7 @@ public class FragmentShowMaps extends Fragment {
 
         // Inicializamos la instancia FirebaseDatabase
         mDatabase = FirebaseDatabase.getInstance().
-                getReference();
+                getReference("SitiosParchaT/Bogota/InfoSitios/Restaurantes");
         geoFireDatabase = FirebaseDatabase.getInstance().
                 getReference("SitiosParchaT/Bogota/GeoFire/Restaurantes");
         geoFire = new GeoFire(geoFireDatabase);
@@ -96,96 +102,25 @@ public class FragmentShowMaps extends Fragment {
             public void onMapReady(@NonNull GoogleMap googleMap) {
                 // Renombramos googleMap por mMap -> literal es como el obejto del mapa de Maps
                 mMap = googleMap;
+                mMap.getUiSettings().setZoomControlsEnabled(true);
 
                 lugares = new ArrayList<>();
                 latLngsLugares = new ArrayList<>();
                 lugaresGeoQueryResult = new ArrayList<>();
+                marcadoresMapa = new ArrayList<>();
+                latLngUbicacionUsuario = null;
 
+                LatLng cargaInicial = new LatLng(4.657160262597251, -74.05605940861399);
 
-                // Creamos un objeto LatLng y le pasamos las coordenadas de Bogota
-                LatLng bogota = new LatLng(4.60971, -74.08175);
-
-                // 4.657160262597251, -74.05605940861399
-
+                // Marcador inicial
                 marcadorPosicion = mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(4.657160262597251, -74.05605940861399))
+                        .position(cargaInicial)
                         .title(4.657 + ", " + -74.056)
                         .visible(true));
-
-                ///
-                mDatabase.child("SitiosParchaT")
-                        .child("Bogota")
-                        .child("InfoSitios")
-                        .child("Restaurantes")
-                        .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull @NotNull Task<DataSnapshot> task) {
-                        if (!task.isSuccessful()) {
-                            Log.e(TAG, "Error recibiendo datos", task.getException());
-                        } else {
-
-                            for (DataSnapshot snapshot : task.getResult().getChildren()) {
-                                // Recibo los datos de un lugar en un objeto de la clase InfoLugar
-                                InfoLugar lugar = snapshot.getValue(InfoLugar.class);
-                                // Añado el lugar a una lista de lugares tipo InfoLugar
-                                lugares.add(lugar);
-                                // Añado latitud y longitud del lugar que se recibio a lista tipo LatLong
-                                LatLng latLngLugar = lugar.getLatLong();
-                                if (latLngLugar != null) {
-                                    latLngsLugares.add(latLngLugar);
-                                    /*geoFire.setLocation(lugar.getNombre(),
-                                            new GeoLocation(latLngLugar.latitude, latLngLugar.longitude),
-                                            new GeoFire.CompletionListener() {
-                                                @Override
-                                                public void onComplete(String key, DatabaseError error) {
-                                                    if (error == null) {
-                                                        Toast.makeText(getContext(), "Bien Geofire", Toast.LENGTH_LONG).show();
-                                                    } else {
-                                                        Toast.makeText(getContext(), "Mal Geofire", Toast.LENGTH_LONG).show();
-                                                    }
-                                                }
-                                            });*/
-                                }
-                            }
-                            // Recorremos el arreglo de objetos LatLng para ubicar y nombrar marcadores con los restauantes en el mapa
-                            for (int x = 0; x < latLngsLugares.size(); x++) {
-                                // Creamos un objeto LatLng y le pasamos las coordenadas de x restaurante
-                                //restaurantes[x] = new LatLng(coordenadas[0][x], coordenadas[1][x]);
-                                //restaurantes[x] = obtenerLatLongDeDireccion(dirRestaurantes[x]);
-                                // Solo si el metodo obtenerLatLongDeDireccion nos devuelve las coordenadas
-                                // vamos a dibujar el marcador
-                                if (latLngsLugares.get(x) != null) {
-                                    InfoLugar lugar = lugares.get(x);
-                                    // Añadimos un marcador al mapa en las coordenadas de x restaurante
-                                    mMap.addMarker(new MarkerOptions().
-                                            position(latLngsLugares.get(x))
-                                            .title(lugar.getNombre())
-                                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_mapa_restaurante))
-                                            .anchor(0.0f, 0.0f));
-                                }
-                                // De lo contrario
-                                else {
-                                    // Enviamos un mensaje emergente diciendo que la conexion fallo
-                                    Toast.makeText(getContext(),
-                                            R.string.msgToast_MainActivity_1,
-                                            Toast.LENGTH_LONG).show();
-                                }
-                            }
-                            // Si el objeto latlong en la posicion 0 del arreglo restaurantes no esta vacio
-                            if (latLngsLugares.get(0) != null) {
-                                // Ubicamos la camara de google maps en el primer restaurante
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngsLugares.get(0), 15));
-                                // SI no
-                            } else {
-                                // Ubicamos la camara de google maps en la ciudad en que viva
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bogota, 11));
-                            }
-                        }
-
-                    }
-                });
-
-                ///
+                latLngUbicacionUsuario = cargaInicial;
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cargaInicial, 15));
+                setQueryOnUsuario(cargaInicial
+                        ,0.5);
 
 
                 mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
@@ -203,14 +138,13 @@ public class FragmentShowMaps extends Fragment {
                         } else {
                             ((MainActivity) getActivity()).iniciarBottomSheetDialog(tituloMarcador, "No disponible en este momento.", "", "");
                         }
-
                         return false;
                     }
                 });
 
                 mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
                     @Override
-                    public void onMapLongClick(@NonNull @NotNull LatLng latLng) {
+                    public void onMapLongClick(@NonNull @NotNull LatLng latLngUsuario) {
 
                         lugaresGeoQueryResult.clear();
 
@@ -218,57 +152,197 @@ public class FragmentShowMaps extends Fragment {
                             marcadorPosicion.remove();
                         }
                         marcadorPosicion = mMap.addMarker(new MarkerOptions()
-                                .position(latLng)
-                                .title(latLng.latitude + ", " + latLng.longitude)
+                                .position(latLngUsuario)
+                                .title(latLngUsuario.latitude + ", " + latLngUsuario.longitude)
                                 .visible(true));
-
-                        // creates a new query around [37.7832, -122.4056] with a radius of 0.6 kilometers
-                        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(latLng.latitude, latLng.longitude), 0.5);
-                        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
-                            @Override
-                            public void onKeyEntered(String key, GeoLocation location) {
-                                Toast.makeText(getContext(), "Bien Geofire, encontramos a " + key + " en un rango de 0.5km", Toast.LENGTH_SHORT).show();
-
-                                lugaresGeoQueryResult.add(key);
-                            }
-
-                            @Override
-                            public void onKeyExited(String key) {
-                                //Toast.makeText(getContext(), "Bien Geofire, onKeyExited", Toast.LENGTH_LONG).show();
-                            }
-
-                            @Override
-                            public void onKeyMoved(String key, GeoLocation location) {
-
-                            }
-
-                            @Override
-                            public void onGeoQueryReady() {
-                                //Toast.makeText(getContext(), "All initial data has been loaded and events have been fired!", Toast.LENGTH_LONG).show();
-                                if (lugaresGeoQueryResult.size() != 0) {
-                                    for (int x = 0; x < lugaresGeoQueryResult.size(); x++) {
-                                        Log.d("Lugar Encontrado", "#" + (x + 1) + " -> " + lugaresGeoQueryResult.get(x));
-                                    }
-                                }
-
-
-
-                            }
-
-                            @Override
-                            public void onGeoQueryError(DatabaseError error) {
-                                Toast.makeText(getContext(), "Error: " + error, Toast.LENGTH_LONG).show();
-
-                            }
-                        });
-
-
+                        // Hacemos la busqueda de los sitios cercanos en un radio x de kilometros
+                        latLngUbicacionUsuario = latLngUsuario;
+                        setQueryOnUsuario(latLngUsuario, 0.4);
                     }
                 });
             }
         });
         return v;
     }
+
+    private void borrarMarcadoresMapa() {
+        // Borramos todos los marcadores que esten en el mapa
+        for (int x = 0; x < marcadoresMapa.size(); x++) {
+            Marker marker = marcadoresMapa.get(x);
+            marker.remove();
+        }
+        // Desocupamos la lista de marcadores
+        marcadoresMapa.clear();
+    }
+
+    private void setQueryOnUsuario(@NonNull LatLng latLngUsuario, double radioEnKm) {
+
+        // creates a new query around [37.7832, -122.4056] with a radius of 0.6 kilometers
+        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(latLngUsuario.latitude,
+                latLngUsuario.longitude), radioEnKm);
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+
+                lugaresGeoQueryResult.add(key);
+
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+                //Toast.makeText(getContext(), "Bien Geofire, onKeyExited", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                //Toast.makeText(getContext(), "All initial data has been loaded and events have been fired!", Toast.LENGTH_LONG).show();
+                if (lugaresGeoQueryResult.size() != 0) {
+                    buscarSitiosCercanos(lugaresGeoQueryResult);
+                    for (int x = 0; x < lugaresGeoQueryResult.size(); x++) {
+                        Log.d("Lugar Encontrado", "#" + (x + 1) + " -> " + lugaresGeoQueryResult.get(x));
+                    }
+                } else {
+                    borrarMarcadoresMapa();
+                    lugares.clear();
+                    latLngsLugares.clear();
+                    Toast.makeText(getContext(), "No encontramos ningun sitio cerca a ti :c, intenta aumentar el rango de busqueda.", Toast.LENGTH_SHORT).show();
+                }
+                // Se buscan los sitios cercanos en la base de datos
+
+
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+                Toast.makeText(getContext(), "Error: " + error, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void addMarkersSitiosCercanos(LatLng latLngUsuario, List<InfoLugar> infoLugarList, List<LatLng> latLngLugarList) {
+
+        // Recorremos el arreglo de objetos LatLng para ubicar y nombrar marcadores con los restauantes en el mapa
+        for (int x = 0; x < latLngLugarList.size(); x++) {
+            // Creamos un objeto LatLng y le pasamos las coordenadas de x restaurante
+            //restaurantes[x] = new LatLng(coordenadas[0][x], coordenadas[1][x]);
+            //restaurantes[x] = obtenerLatLongDeDireccion(dirRestaurantes[x]);
+            // Solo si el metodo obtenerLatLongDeDireccion nos devuelve las coordenadas
+            // vamos a dibujar el marcador
+            if (latLngLugarList.get(x) != null) {
+                // Creamos un objeto tipo Marker
+                Marker marcadorSitioCercano;
+                InfoLugar lugar = infoLugarList.get(x);
+                // Añadimos un marcador al mapa en las coordenadas de x restaurante
+                marcadorSitioCercano = mMap.addMarker(new MarkerOptions()
+                        .position(latLngLugarList.get(x))
+                        .title(lugar.getNombre())
+                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_mapa_restaurante))
+                        .anchor(0.0f, 0.0f));
+                // Añadimos el marcador a la lista de marcadores para luego borrarlos
+                marcadoresMapa.add(marcadorSitioCercano);
+            }
+            // De lo contrario
+            else {
+                // Enviamos un mensaje emergente diciendo que la conexion fallo
+                Toast.makeText(getContext(),
+                        R.string.msgToast_MainActivity_1,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+        // Si el objeto latlong en la posicion 0 del arreglo restaurantes no esta vacio
+        if (latLngUsuario != null) {
+            // Ubicamos la camara de google maps en el primer restaurante
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngUsuario, 15));
+            // SI no
+        } else {
+            // Ubicamos la camara de google maps en la ciudad en que viva
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bogota, 11));
+        }
+    }
+
+    private void buscarSitiosCercanos(List<String> lugaresQueryGeoFire) {
+
+        borrarMarcadoresMapa();
+        lugares.clear();
+        latLngsLugares.clear();
+
+        // Obtenemos la informacion de los lugares que arrojo el Query
+        // de la base de datos
+
+            mDatabase.get()
+                    .addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DataSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (DataSnapshot snapshot : task.getResult().getChildren()) {
+                                    InfoLugar infoLugar = snapshot.getValue(InfoLugar.class);
+                                    if (infoLugar != null) {
+                                        for (int x = 0; x < lugaresQueryGeoFire.size(); x++) {
+                                            String nomlugarQueryGeoFire = lugaresQueryGeoFire.get(x);
+                                            String nomlugarDB = infoLugar.getNombre();
+                                            if (nomlugarQueryGeoFire.equals(nomlugarDB)) {
+                                                addASitiosCercanos(infoLugar);
+                                            }
+                                        }
+                                    }
+                                }
+                                if ((lugares.size()) == lugaresQueryGeoFire.size()) {
+                                    getSitiosCercanosConExito();
+                                }
+                            } else {
+                                getSitiosCercanosConFalla();
+                            }
+                        }
+                    });
+
+
+    }
+
+    private void getSitiosCercanosConFalla() {
+
+    }
+
+    private void getSitiosCercanosConExito() {
+        // Obtenemos las listas de objetos InfoLugar y LataLong
+        LatLng latLngUsuario = getLatLongUsuario();
+        List<InfoLugar> infoLugarList = getListInfoLugarDeSitiosCercanos();
+        List<LatLng> latLngLugarList = getListLatLongDeDeSitiosCercanos();
+        if (infoLugarList != null && latLngLugarList != null) {
+            if (infoLugarList.size() != 0 && latLngLugarList.size() != 0) {
+                if (latLngUsuario != null) {
+                    addMarkersSitiosCercanos(latLngUsuario, infoLugarList, latLngLugarList);
+                }
+            }
+        }
+    }
+
+    private void addASitiosCercanos(InfoLugar infoLugar) {
+        // Añado el lugar a una lista de lugares tipo InfoLugar
+        lugares.add(infoLugar);
+        // Añado latitud y longitud del lugar que se recibio a lista tipo LatLong
+        LatLng latLngLugar = infoLugar.getLatLong();
+        if (latLngLugar != null) {
+            latLngsLugares.add(latLngLugar);
+        }
+    }
+
+    private LatLng getLatLongUsuario() {
+        return latLngUbicacionUsuario;
+    }
+
+    private List<InfoLugar> getListInfoLugarDeSitiosCercanos(){
+        return lugares;
+    }
+
+    private List<LatLng> getListLatLongDeDeSitiosCercanos(){
+        return latLngsLugares;
+    }
+
 
     private InfoLugar getInfoLugarSeleccionado(String tituloMarcador) {
         InfoLugar lugar = null;
@@ -281,3 +355,16 @@ public class FragmentShowMaps extends Fragment {
         return lugar;
     }
 }
+
+/*geoFire.setLocation(lugar.getNombre(),
+new GeoLocation(latLngLugar.latitude, latLngLugar.longitude),
+new GeoFire.CompletionListener() {
+    @Override
+    public void onComplete(String key, DatabaseError error) {
+        if (error == null) {
+            Toast.makeText(getContext(), "Bien Geofire", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getContext(), "Mal Geofire", Toast.LENGTH_LONG).show();
+        }
+    }
+});*/
